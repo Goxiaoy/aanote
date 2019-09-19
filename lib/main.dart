3fa600be38fb3d5f839795aabe9b7932c9cf260b
goxiaoy <xy1996mail@gmail.com>
2019-09-19 21:40:34 +0800

    add locale

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:aanote/main_page.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'generated/i18n.dart';
+
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
+
+  
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      localizationsDelegates: [
+        S.delegate,
+        // ... app-specific localization delegate[s] here
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
