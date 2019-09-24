aa6eedf274e20bd642c4b8da1f988c151e1f9303
goxiaoy <xy1996mail@gmail.com>
2019-09-24 23:51:27 +0800

    add sqflite

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,20 @@ import 'package:aanote/main_page.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'generated/i18n.dart';
+import 'package:aanote/utils/environment.dart';
+import 'package:sqflite/sqflite.dart';
 
-void main() => runApp(new MyApp());
+void main() {
+  if(!inProduction){
+    Sqflite.devSetDebugModeOn(true);
+  }
+  runApp(new MyApp());
+}
 
 class MyApp extends StatelessWidget {
 
-  
+
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
